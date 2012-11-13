3fbb2fda2eb4e38e37dedc784f74c5df5fbe2c0a
Jan Willem van Diermen <jwvdiermen@crealuz.nl>
2012-11-14 00:37:05 +0100

    Updated default API settings.

--- src/ApiSettings.cs
+++ src/ApiSettings.cs
@@ -16,37 +16,11 @@ namespace TransIp.Api.Examples.Shared
 		/// <summary>
 		/// The login.
 		/// </summary>
-		public static readonly string Login = "jwvdiermen";
+		public static readonly string Login = "";
 
 		/// <summary>
 		/// The private key.
 		/// </summary>
-		public static readonly string PrivateKey = @"-----BEGIN RSA PRIVATE KEY-----
-MIIEpAIBAAKCAQEAzmF5dRwIDondpKnOZMIC2ew4nLlaMLvCZ3283mysD9z5Brea
-YIoRvQJt725AuA8CffDX5UrUeOuaCpmXH7+QL3/tcucAVBwJ5RzKrUpmSSPIXUJQ
-qXhz1MFIS6QHPtWpRnL5V2Wo9d6vgWU7PFKGqQfAJUzmtDfVsUbUM7BrT7EvWGUX
-+rnJD3LixK9dWcPVy3O1nmazKJNjGALyaaTJDEwCIRMp6VyMfvG+6/XXhlYR3S03
-eq8pGXVqSqs7JbZPMVyctXf111Y+ejMqiNlZmCwfpYwlJbPu+KZsLttlEfqWDpQE
-4dSDcsQlECTomu6jFS9zRrTmL2cmf8zkkVSPjQIDAQABAoIBAQCKd8Xo+ATD1GY8
-a53J5o3JLv+Qz5+eoOs/SpKk3V7YSddfVWKjsR9TpESkZ2HO3Gs8mpIQCpPiCZlR
-0Vke+QVBiWCEPk7vH9zXtuiZOhjEq9hsEelSuzlkHsZl0cj2tJ4dYVi/9bpWNLGm
-bIhA4dHtqQCXRjBn7CpZBf+sKorlbPSmaIKaMFjUDRAoZWCnUZ1/L7PQCmTCk/Xu
-XIecsdiy5WYRQnoAyxulrtC2MVnmXeIIpDqcMwZwF/tzhSHT/H3tCEi99zgaE3dj
-+2YSXfcKaESqMb+DE7yO09LnhKez1cEog3JV/PzQIEjP7JvhIICZL4bGpkP+AW51
-hHe08mcBAoGBAOr10kUQ+t4pDmB0Vnus0X9pgHfF2sXIZZDKP1P8EbtmJ8RtFBOe
-/hvcwC1+EAjy+gJE5qsFz/2/2yMZIFEov5Sbmms1Qpslo6E+0w9OOWeW8xyb9dCV
-7dp/gNZCxj0rVsdKOR1wgV4TicZUT3fyu8TlLscCzG4gVD6oYDYW41LNAoGBAODc
-gM3+xnN/633IGl7H6CZkkyG3I343T17uyW9p9kyKOXNrRrg79XFwL4Q1GoBhuOt3
-VUPn5kY5SDyiocfnxQ/MgHVQcd4xwWq3rT9wADdC56kOMiQSL37eCkmp1KQKfrov
-QuUbAu/rW4zTT76Oo0fzvncTA7fWZ9odDOztva/BAoGAKegoVcs+g2tdNhTp6+sZ
-/pipojM23vnsK5P3EZqu6vbAdwdhglJkTkHkQPjwETiNIOR7I9vIiiCzDCKKIg+b
-g/zw4NhCBfwDoFndOSihknlY6Sxj/o0PPF5rc0u7oeNd+fOiFj8fw9DGTQpylhlE
-Jk0eN76nCalYfUh4yIzyhK0CgYB/eIEMRgH6N+onw+gvEuRn31wJIOjeBDzadEN9
-BXS6ryEibQ4KIvNg+1f0eqYrYTqTQXL0q+G+rXpl5UwRJzJvYl7wIkpqy4n6FWYB
-MFzu9t6c149VI3oJUZZDbCM/WzO8GE6z0jw4BhRAIQpz3Ch0AZlXp0/UR5dX7mAF
-cEC4AQKBgQCCvztBn38T1CiC4X5xS8HZe1wqPXhaBX53/wnr2APM764VSGXCsJO/
-P6e3Hd2mi4E/OCEkq3Wn76B0ruyndSHgu2NITfTUdaZiaRhmom5tcaQhrps+lomP
-6YNatcJ1NRUjBEAbx6GUTdd+7kpAmfSjCp0FpgjiXW+73AHZs3M1eA==
------END RSA PRIVATE KEY-----";
+		public static readonly string PrivateKey = @"";
 	}
 }
